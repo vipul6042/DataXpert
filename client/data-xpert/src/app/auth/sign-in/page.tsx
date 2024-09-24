2a141e02f1e5649561da8797af57bc9f63d5c8b2
hustlernik <hustlernikxd@gmail.com>
2024-09-25 00:07:55 +0530

    client setup

---
+++ client/data-xpert/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+import SingIn from "./SignIn.js";
+
+export default function SignIn() {
+	return (
+		<div className="flex flex-col justify-center items-center h-screen gap-[20px]">
+			<h1>hello</h1>
+			<SignIn />
+		</div>
+	);
+}
--- client/data-xpert/src/app/page.tsx
+++ client/data-xpert/src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="flex justify-center items-center h-screen">
+		<div className="flex flex-col justify-center items-center h-screen gap-[20px]">
 			<h1 className="font-bold text-5xl">DataXpert</h1>
+			<div className="flex gap-10">
+				<Link href="/auth/sign-in">
+					<button
+						type="button"
+						className="bg-black text-white rounded-[10px] h-[40px] w-[100px] font-bold"
+					>
+						sign-in
+					</button>
+				</Link>
+
+				<Link href="/auth/sign-up">
+					<button
+						type="button"
+						className="bg-black text-white rounded-[10px] h-[40px] w-[100px] font-bold"
+					>
+						sign-up
+					</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
