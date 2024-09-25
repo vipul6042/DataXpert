c8bd8bfb5d57577f9527c709873eee1dda051f3d
vipul <vipulkumar2426@gmail.com>
2024-09-25 15:53:30 +0530

    database

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,16 @@
-import { Pool } from "pg";
-require("dotenv").config();
-
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+import { Sequelize } from 'sequelize';
+import dotenv from "dotenv";
+dotenv.config();
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres', // or 'mysql', 'sqlite', etc.
+    port:process.env.DB_PORT||11078,
+    dialectOptions: {
+        ssl: {
+            require: true,        // Enable SSL
+            rejectUnauthorized: false, // Depending on your setup, this might need to be true
+        },
+    },
 });
 
-module.exports = pool;
+export { sequelize };
